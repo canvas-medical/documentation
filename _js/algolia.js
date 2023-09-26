1f7804ea75e224ffc3504997dfadb06faef77965
Cylas Tee <chin_yue_tee@hotmail.com>
2023-09-26 22:34:02 +0800

    Update Search Button, Search Result, Search Container

--- _js/algolia.js
+++ _js/algolia.js
@@ -1,50 +1,54 @@
 import { algoliaId, algoliaIndex, algoliaSearchKey } from './apikeys';
 
 window.addEventListener('load', () => {
-  const searchClient = algoliasearch(algoliaId, algoliaSearchKey);
-  const inputclear = document.querySelector('.ais-SearchBox-input');
+    const searchClient = algoliasearch(algoliaId, algoliaSearchKey);
+    const inputclear = document.querySelector('.ais-SearchBox-input');
 
-  const search = instantsearch({
-    indexName: algoliaIndex,
-    searchClient,
+    const search = instantsearch({
+        indexName: algoliaIndex,
+        searchClient,
 
-    searchFunction(helper) {
-      const searchResults = document.getElementById('search_container');
-      if (helper.state.query === '' && searchResults.style.display === 'none' && overlay.style.display === 'none') {
-        return;
-      }
-      searchResults.style.display = helper.state.query ? 'grid ' : 'none';
+        searchFunction(helper) {
+            const searchResults = document.getElementById('search_container');
+            if (helper.state.query === '' && searchResults.style.display === 'none' && overlay.style.display === 'none') {
+                return;
+            }
+            searchResults.style.display = helper.state.query ? 'grid ' : 'none';
 
-      helper.search();
-    },
-  });
-  const refinementWidget = instantsearch.widgets.refinementList({
-    container: '#refinementlist',
-    attribute: 'collection',
-  });
+            helper.search();
+        },
+    });
+    const refinementWidget = instantsearch.widgets.refinementList({
+        container: '#refinementlist',
+        attribute: 'collection',
+    });
 
-  let timerId;
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: '#algolia-search',
-      placeholder: 'Search',
-      showLoadingIndicator: true,
-      queryHook(query, refine) {
-        clearTimeout(timerId);
-        timerId = setTimeout(() => refine(query), 500);
-      },
-    }),
-  );
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#algolia-results',
+    let timerId;
+    search.addWidget(
+        instantsearch.widgets.searchBox({
+            container: '#algolia-search',
+            placeholder: 'Search',
+            showLoadingIndicator: true,
+            queryHook(query, refine) {
+                clearTimeout(timerId);
+                timerId = setTimeout(() => refine(query), 500);
+            },
+        }),
+    );
+    search.addWidget(
+        instantsearch.widgets.hits({
+            container: '#algolia-results',
 
-      templates: {
-        item(hit, { html, components }) {
-          return html`
+            templates: {
+                item(hit, { html, components }) {
+                    return html `
             <div class="post-item">            
             ${components.Highlight({ attribute: 'name', hit })}
-              <a class="post-link" href="${hit.url}">${hit.title}
+              <a class="post-link" href="${hit.url}">
+              <div class="post-title-container"> 
+                <div class="post-title">${hit.title}</div>
+                <div class="post-collection">${hit.collection}</div>
+              </div>
               <div class="post-snippet"><p>${hit.content}</p></div>
               </a>
               
@@ -52,22 +56,22 @@ window.addEventListener('load', () => {
 
               
             `;
-        },
-      },
-    }),
-  );
+                },
+            },
+        }),
+    );
 
-  search.addWidget(
-    instantsearch.widgets.pagination({
-      container: '#pagination',
-      maxPages: 20,
-      // default is to scroll to 'body', we disable this behavior
-      scrollTo: false,
-      showFirstLast: false,
-    }),
-  );
+    search.addWidget(
+        instantsearch.widgets.pagination({
+            container: '#pagination',
+            maxPages: 20,
+            // default is to scroll to 'body', we disable this behavior
+            scrollTo: false,
+            showFirstLast: false,
+        }),
+    );
 
-  search.addWidgets([refinementWidget]);
+    search.addWidgets([refinementWidget]);
 
-  search.start();
+    search.start();
 });
