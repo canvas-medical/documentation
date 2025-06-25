a4aa2e5559f5a4302f990c6e0df32200569e9a09
Andrew Duane <andrew.duane@canvasmedical.com>
2025-06-24 18:13:03 -0700

    Use LLM to describe python files included with the plugin

--- scripts/sdk/generate_example_plugins_docs.rb
+++ scripts/sdk/generate_example_plugins_docs.rb
@@ -1,5 +1,6 @@
-require 'pathname'
+require "net/http"
 require 'json'
+require 'pathname'
 
 #
 # Usage: ruby scripts/sdk/generate_example_plugins_docs.rb api_samples,example_chart_app,plugins_smoke_test
@@ -11,7 +12,9 @@ require 'json'
 # will be invoked from the root of the documentation repo, so the example
 # plugins will be found at a relative path of
 # './canvas-plugins/example-plugins/'
-canvas_plugins_repo_root = ENV['EXAMPLE_PLUGINS_ROOT'] || './canvas-plugins/example-plugins/'
+CANVAS_PLUGINS_REPO_ROOT = ENV['EXAMPLE_PLUGINS_ROOT'] || './canvas-plugins/example-plugins/'
+
+OPENAI_API_KEY = ENV['OPENAI_API_KEY']
 
 # List the plugins we were requested to generate docs for
 changed_plugins = ARGV[0].split(',')
@@ -21,10 +24,65 @@ changed_plugins.each do |plugin|
 end
 puts
 
+# Define a method to get an external consultation on what a file's purpose is
+def get_llm_explanation_of_python_code(python_file)
+  filename = python_file.basename.to_s
+  file_content = python_file.readlines.join
+
+  # Fake the output if we don't have an API key set (for debugging/local dev)
+  if OPENAI_API_KEY.nil?
+    response_json = [
+        {
+            "id": "msg_abc123",
+            "type": "message",
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "output_text",
+                    "text": "This file is great.",
+                    "annotations": []
+                }
+            ]
+        },
+        {
+            "id": "msg_def456",
+            "type": "message",
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "output_text",
+                    "text": "It does lots of neat stuff.",
+                    "annotations": []
+                }
+            ]
+        }
+    ]
+  else
+    uri = URI('https://api.openai.com/v1/responses')
+
+    body = {
+      instructions: "This is #{filename}. It is included in a plugin built using the Canvas SDK, whose documentation can be found at https://docs.canvasmedical.com. What does the code in this file do? Give me markdown output, but with no headers or horizontal lines. Instead of headers, separate sections with bold titles.",
+      model: 'gpt-4.1',
+      input: file_content
+    }
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer #{OPENAI_API_KEY}"
+    }
+
+    response = Net::HTTP.post(uri, body.to_json, headers)
+    response_json = JSON.parse(response.body)
+  end
+
+  outputs = response_json['output'].map{|output| output['content'].select{|content| content['type'] == 'output_text'}.map{|output| output['text']}.join("\n")}.join("\n")
+  return outputs
+end
+
 # For each plugin
 changed_plugins.each do |plugin_name|
   # See if there is a matching plugin directory in the canvas plugins repo
-  plugin_path = Pathname.new("#{canvas_plugins_repo_root}#{plugin_name}")
+  plugin_path = Pathname.new("#{CANVAS_PLUGINS_REPO_ROOT}#{plugin_name}")
   unless plugin_path.exist?
     puts "Plugin not found: #{plugin_path}"
     next
@@ -95,21 +153,23 @@ ALERTHTML
           # If its the manifest file, ensure the syntax highlighting is set to
           # json when dumping the contents.
           if child_path.basename.to_s.downcase == 'canvas_manifest.json'
-              file.puts("```json")
-              child_path.readlines.each do |line|
-                file.puts(line)
-              end
-              file.puts("```")
-              file.puts()
+            file.puts("```json")
+            child_path.readlines.each do |line|
+              file.puts(line)
+            end
+            file.puts("```")
+            file.puts()
           # If it's a python file, ensure the syntax highlighting is set to
           # python when dumping the contents
           elsif child_path.extname == ".py"
-              file.puts("```python")
-              child_path.readlines.each do |line|
-                file.puts(line)
-              end
-              file.puts("```")
-              file.puts()
+            file.puts(get_llm_explanation_of_python_code(child_path))
+            file.puts()
+            file.puts("```python")
+            child_path.readlines.each do |line|
+              file.puts(line)
+            end
+            file.puts("```")
+            file.puts()
           end
         end
       end
