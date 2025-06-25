ffea07266492dd1b14da700dd6c93b4156a61cef
Andrew Duane <andrew.duane@canvasmedical.com>
2025-06-25 14:02:12 -0700

    Remove mocked OpenAI response

--- scripts/sdk/generate_example_plugins_docs.rb
+++ scripts/sdk/generate_example_plugins_docs.rb
@@ -29,51 +29,23 @@ def get_llm_explanation_of_python_code(python_file)
   filename = python_file.basename.to_s
   file_content = python_file.readlines.join
 
-  # Fake the output if we don't have an API key set (for debugging/local dev)
-  if OPENAI_API_KEY.nil?
-    response_json = [
-        {
-            "id": "msg_abc123",
-            "type": "message",
-            "role": "assistant",
-            "content": [
-                {
-                    "type": "output_text",
-                    "text": "This file is great.",
-                    "annotations": []
-                }
-            ]
-        },
-        {
-            "id": "msg_def456",
-            "type": "message",
-            "role": "assistant",
-            "content": [
-                {
-                    "type": "output_text",
-                    "text": "It does lots of neat stuff.",
-                    "annotations": []
-                }
-            ]
-        }
-    ]
-  else
-    uri = URI('https://api.openai.com/v1/responses')
+  raise "You must set OPENAI_API_KEY" if OPENAI_API_KEY.nil?
 
-    body = {
-      instructions: "This is #{filename}. It is included in a plugin built using the Canvas SDK, whose documentation can be found at https://docs.canvasmedical.com. What does the code in this file do? Give me markdown output, but with no headers or horizontal lines. Instead of headers, separate sections with bold titles.",
-      model: 'gpt-4.1',
-      input: file_content
-    }
+  uri = URI('https://api.openai.com/v1/responses')
 
-    headers = {
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer #{OPENAI_API_KEY}"
-    }
+  body = {
+    instructions: "This is #{filename}. It is included in a plugin built using the Canvas SDK, whose documentation can be found at https://docs.canvasmedical.com. What does the code in this file do? Give me markdown output, but with no headers or horizontal lines. Instead of headers, separate sections with bold titles.",
+    model: 'gpt-4.1',
+    input: file_content
+  }
 
-    response = Net::HTTP.post(uri, body.to_json, headers)
-    response_json = JSON.parse(response.body)
-  end
+  headers = {
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer #{OPENAI_API_KEY}"
+  }
+
+  response = Net::HTTP.post(uri, body.to_json, headers)
+  response_json = JSON.parse(response.body)
 
   outputs = response_json['output'].map{|output| output['content'].select{|content| content['type'] == 'output_text'}.map{|output| output['text']}.join("\n")}.join("\n")
   return outputs
